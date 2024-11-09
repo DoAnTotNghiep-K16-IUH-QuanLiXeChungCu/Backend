4bebb05ad30bf1c45a1a4c5dd5b7df1a308854f8
TranThienDatLaGi <tranthiendatlagi@gmail.com>
2024-11-09 22:12:55 +0700

    modifiled entryRecordController and entryRecordRoute

--- routes/EntryRecordRoute.js
+++ routes/EntryRecordRoute.js
@@ -4,34 +4,70 @@ const middleware = require("../middleware/middlewareController");
 const router = Router();
 
 // http://localhost:3000/api/v1/entryRecord/GetAllEntryRecords
-router.patch("/GetAllEntryRecords", middleware.verifyToken , EntryRecordController.GetAllEntryRecords);
+router.patch(
+  "/GetAllEntryRecords",
+  middleware.verifyToken,
+  EntryRecordController.GetAllEntryRecords
+);
 
 // http://localhost:3000/api/v1/entryRecord/GetEntryRecordById
-router.patch("/GetEntryRecordById", middleware.verifyToken , EntryRecordController.GetEntryRecordById);
+router.patch(
+  "/GetEntryRecordById",
+  middleware.verifyToken,
+  EntryRecordController.GetEntryRecordById
+);
 
 // http://localhost:3000/api/v1/entryRecord/GetEntryRecordByLicensePlate
-router.patch("/GetEntryRecordByLicensePlate", middleware.verifyToken , EntryRecordController.GetEntryRecordByLicensePlate);
+router.patch(
+  "/GetEntryRecordByLicensePlate",
+  middleware.verifyToken,
+  EntryRecordController.GetEntryRecordByLicensePlate
+);
 
 // http://localhost:3000/api/v1/entryRecord/GetEntryRecordsByDateRange
-router.patch("/GetEntryRecordsByDateRange", middleware.verifyToken , EntryRecordController.GetEntryRecordsByDateRange);
+router.patch(
+  "/GetEntryRecordsByDateRange",
+  middleware.verifyToken,
+  EntryRecordController.GetEntryRecordsByDateRange
+);
 
 // http://localhost:3000/api/v1/entryRecord/GetEntryRecordsByVehicleType
-router.patch("/GetEntryRecordsByVehicleType", middleware.verifyToken , EntryRecordController.GetEntryRecordsByVehicleType);
+router.patch(
+  "/GetEntryRecordsByVehicleType",
+  middleware.verifyToken,
+  EntryRecordController.GetEntryRecordsByVehicleType
+);
 
 // http://localhost:3000/api/v1/entryRecord/CountVehicleEntry
-router.patch("/CountVehicleEntry", middleware.verifyToken , EntryRecordController.CountVehicleEntry);
+router.patch(
+  "/CountVehicleEntry",
+  middleware.verifyToken,
+  EntryRecordController.CountVehicleEntry
+);
 
 // http://localhost:3000/api/v1/entryRecord/CreateEntryRecord
-router.post("/CreateEntryRecord",EntryRecordController.CreateEntryRecord);
+router.post("/CreateEntryRecord", EntryRecordController.CreateEntryRecord);
 
 // http://localhost:3000/api/v1/entryRecord/CountVehicleNonExit
-router.get("/CountVehicleNonExit", middleware.verifyToken , EntryRecordController.CountVehicleNonExit);
+router.patch(
+  "/CountVehicleNonExit",
+  middleware.verifyToken,
+  EntryRecordController.CountVehicleNonExit
+);
 
 // http://localhost:3000/api/v1/entryRecord/FilterEntryRecords
-router.patch("/FilterEntryRecords", middleware.verifyToken , EntryRecordController.FilterEntryRecords);
+router.patch(
+  "/FilterEntryRecords",
+  middleware.verifyToken,
+  EntryRecordController.FilterEntryRecords
+);
 
 // http://localhost:3000/api/v1/entryRecord/GetEntryRecordByisOutAndUuidAndLicensePlate
-router.patch("/GetEntryRecordByisOutAndUuidAndLicensePlate", middleware.verifyToken , EntryRecordController.GetEntryRecordByisOutAndUuidAndLicensePlate);
+router.patch(
+  "/GetEntryRecordByisOutAndUuidAndLicensePlate",
+  middleware.verifyToken,
+  EntryRecordController.GetEntryRecordByisOutAndUuidAndLicensePlate
+);
 
 // Đảm bảo bạn export router đúng cách
-module.exports = router; 
+module.exports = router;
